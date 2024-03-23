f19ba78c50a9139a96de508c3f4831b3925803f7
gdjaveria <javeriajiapk2gmail.com>
2024-03-23 15:51:23 -0700

    add my commit

---
+++ index.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+import inquirer from 'inquirer';
+const answer = await inquirer.prompt([]);
+// computer generate a random number.
+// user input for guessing number.
+// compare user input to computer generated number and show result.
+
+const randomNumber = Math.floor( Math.random()* 10 + 1);
+
+const answers = await inquirer.prompt([{
+    name: "userGuessedNumber",
+    type: "number",
+    message: "please guess a right number between 1-10:",
+
+}]);
+console.log(answers);
+
+if(answers.userGuessedNumber === randomNumber){
+    console.log("Congtratulations! you guessed right number.");
+} else{
+    console.log("You guessed wrong number");
+}
+
